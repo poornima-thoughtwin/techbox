b1a478fdd599bbac1eba05a063b386ae7fbc3d5c
poornima-thoughtwin <83641756+poornima-thoughtwin@users.noreply.github.com>
2021-05-12 13:01:07 +0530

    Update settings.py

--- tech_box/settings.py
+++ tech_box/settings.py
@@ -199,9 +199,19 @@ LANGUAGES = [
 
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = "static"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = "static"
 MEDIA_URL ="/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 LOGIN_REDIRECT_URL="/index/"
