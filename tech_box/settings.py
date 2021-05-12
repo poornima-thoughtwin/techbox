5c63d1c98a86807dab04e1364369ff887d50ce57
poornima-thoughtwin <83641756+poornima-thoughtwin@users.noreply.github.com>
2021-05-12 12:36:21 +0530

    Update settings.py

--- tech_box/settings.py
+++ tech_box/settings.py
@@ -201,7 +201,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = "static"
 MEDIA_URL ="/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 LOGIN_REDIRECT_URL="/index/"
